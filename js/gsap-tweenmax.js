8998e627d5a9a65aa0463c1920b63ec1f282ff4d
phits <john@fitz.com>
2017-01-29 17:29:51 -0500

    GSAP and TweeenMax Intro complete

---
+++ js/gsap-tweenmax.js
@@ -0,0 +1,12 @@
+
+//TweenMax.to('.bird', .5, {marginLeft: 700, width: 300});
+
+//TweenMax.from('.bird', .5, {marginLeft: 700, width: 300});
+
+//TweenMax.fromTo('.bird', .5, {marginLeft: 700, width: 300}, { marginLeft: 400, width: 100});
+
+//TweenMax.to('.bird', .5, {marginLeft: 700});
+
+//TweenMax.staggerTo('.bird', .5, {marginLeft: 700}, 0.2
+
+TweenMax.staggerFrom('.bird', .5, {marginLeft: 700}, 0.2);
