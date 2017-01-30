b5c1ba8c5c4b002726db8af7c776fdb55bed5031
phits <john@fitz.com>
2017-01-30 10:54:23 -0500

    Anim css3 transforms

---
+++ js/anim-css3-trans.js
@@ -0,0 +1,13 @@
+
+
+TweenMax.to('.square', .75, {
+    // x: 700,
+    // y: 500,
+    // scaleX: 2, transformOrigin: "left top"
+    // scaleX: 2, transformOrigin: "700px top"
+    // rotation: 360, transformOrigin: "left top"
+    // rotationX: 360, transformOrigin: "left 50% -400"
+    // boxShadow: "20px 20px 20px #f00"
+    // boxShadow: "0px 0px 40px #f00"
+    borderRadius: '50px 0px 10px 5px'
+});
